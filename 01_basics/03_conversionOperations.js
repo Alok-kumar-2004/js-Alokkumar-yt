ee2c64d4b245b10200580610c82dbd0cd5ff44e9
Alok-kumar-2004 <137861383+Alok-kumar-2004@users.noreply.github.com>
2024-06-19 12:54:26 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score ="33abc"//=>NaN
+//let score =null=>0
+//let score =undefiend=>NaN
+//let score ="true"=>1 & false=>0
+
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let loggedIn=44
+//let loggedIn="alokkumar"=>true
+// let loggedIn=0 =>false
+// let loggedIn=1 =>True
+// let booleanIsLoggedIn=Boolean(loggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+let someNumber = 63
+let stringNumber=String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
