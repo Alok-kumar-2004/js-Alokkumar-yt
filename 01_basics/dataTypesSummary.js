c6dd486ee96004a222bf7069acc54c1be4c9d939
Alok-kumar-2004 <137861383+Alok-kumar-2004@users.noreply.github.com>
2024-06-19 16:34:12 +0000

    summary done

---
+++ 01_basics/dataTypesSummary.js
@@ -0,0 +1,47 @@
+//Primitive Datatypes
+    //    Number => number
+    //    String  => string
+    //    Boolean  => boolean
+    //    null  => object
+    //    undefined  => undefined
+    //    Symbol  =>  symbol
+    //    BigInt  =>  bigint
+
+// Non-primitive Datatypes
+    //    Arrays  =>  object
+    //    Function  =>  function
+    //    Object  =>  object
+
+    const score=100
+    const scoreValue=100.3
+    const loggedIn=false
+    const Temp =null
+    let userEmail;
+
+
+
+    // const id =Symbol('123')
+    // const anotherId=Symbol('123')
+
+    // console.log(id==anotherId);
+    // console.log(id);
+    // console.log(anotherId);
+
+
+    // const bigNumber=655352728355117n
+    // console.log(bigNumber);
+
+    console.log(typeof bigNumber);
+    console.log(typeof Temp);
+
+    myObj={
+        name:"Alokkumar",
+        age:19,
+        city:"Mumbai"
+    }
+
+    const myFunction = function (){
+        console.log("Hello World");
+    }
+
+    console.log(typeof myFunction);
