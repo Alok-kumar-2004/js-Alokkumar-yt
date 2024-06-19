f784efbaa3b7efb01231dcf6c519af1c0a68c07e
Alok-kumar-2004 <137861383+Alok-kumar-2004@users.noreply.github.com>
2024-06-19 16:33:42 +0000

    comparision done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,30 @@
+//console.log(2!=1);
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+// console.log(null===0);
+// console.log(null<=0);
+
+// comparision and equality check work differently  comparision convert 
+// null to number treating it as zero
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+
+//===  checks value strictly i.e. it also checks its datatype
+//==  checks only value.
+
+// console.log("2"===2);
+// console.log(2>true);
+// console.log(2>=true);
+// console.log(2<=true);
+// console.log(0<="");
+// console.log(0>="");
+// console.log(0>"");
