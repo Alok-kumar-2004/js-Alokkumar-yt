b74bc98a1326540267da9bc62d3f25897a91b3c8
Alok-kumar-2004 <137861383+Alok-kumar-2004@users.noreply.github.com>
2024-06-19 18:18:49 +0000

    summaryOndataTypes

--- 01_basics/dataTypesSummary.js
+++ 01_basics/dataTypesSummary.js
@@ -17,7 +17,9 @@
     const loggedIn=false
     const Temp =null
     let userEmail;
-
+    const heros = ["shaktiman", "naagraj", "doga"];
+    console.log(typeof(heros));
+    console.log(heros);
 
 
     // const id =Symbol('123')
@@ -31,17 +33,39 @@
     // const bigNumber=655352728355117n
     // console.log(bigNumber);
 
-    console.log(typeof bigNumber);
-    console.log(typeof Temp);
+    // console.log(typeof bigNumber);
+    // console.log(typeof Temp);
 
-    myObj={
-        name:"Alokkumar",
-        age:19,
-        city:"Mumbai"
-    }
+    // myObj={
+    //     name:"Alokkumar",
+    //     age:19,
+    //     city:"Mumbai"
+    // }
 
-    const myFunction = function (){
-        console.log("Hello World");
-    }
+    // const myFunction = function (){
+    //     console.log("Hello World");
+    // }
 
-    console.log(typeof myFunction);
+    // console.log(typeof myFunction);
+
+  //  stack(Primitive) , Heap(Non-primitive)
+
+  let myYoutubename="Alokkumardotcom"
+  let anothername=myYoutubename
+  anothername="chaiAurCode"
+
+  console.log(myYoutubename);
+  console.log(anothername);
+
+  let userOne={
+    name:"Alokkumar",
+    age:19,
+    city:"Mumbai"
+  }
+
+  console.log(userOne.age);
+let userTwo=userOne
+
+userTwo.age= 32
+  console.log(userOne.age)
+  console.log(userTwo.age)
