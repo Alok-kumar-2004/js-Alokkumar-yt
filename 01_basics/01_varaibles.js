a210c9464d914170e279457a52c55460847cc1dc
Alok-kumar-2004 <137861383+Alok-kumar-2004@users.noreply.github.com>
2024-06-19 12:31:09 +0000

    variables done

---
+++ 01_basics/01_varaibles.js
@@ -0,0 +1,18 @@
+const accountId=245343
+let accountEmail="alok@608gmail.com"
+var accountpassword="12345"
+accountCity="Mumbai"
+let accountState;
+
+// accountId=23233  // not Allowed only for const....
+console.log(accountId)
+
+
+accountEmail="alokkumar@608gmail.com"
+accountpassword="1333345"
+accountCity="Delhi"
+
+
+console.table([accountEmail,accountpassword,accountCity,accountEmail,accountState])
+
+// prefer not use Var keyword because of block scope and Functional Scope
