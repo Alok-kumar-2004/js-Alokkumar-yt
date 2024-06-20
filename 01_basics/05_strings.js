adff0b76b2a1d729c8c2b5ae5c2063c001f2c15d
Alok-kumar-2004 <137861383+Alok-kumar-2004@users.noreply.github.com>
2024-06-20 16:38:29 +0000

    Strings done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,49 @@
+const name = "Alokkumar"
+const repoCount= 50
+
+//console.log(name+repoCount);
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName=new String('kill-shot-kumar')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.padStart());
+
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('r'));
+
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+// const anotherString=gameName.slice(-13,6)
+// console.log(anotherString);
+
+// const newStringOne="  Alok kumar singh  "
+//console.log(newStringOne.trim()); // also there are method =>trimstart and trimend 
+
+// const url="https://alokkumar.com/kumar%20singh"
+// console.log(url.replace('%20','--'))
+
+
+// console.log(url.includes('alokkumar'))
+// console.log(gameName.split('-'));
+// console.log(newStringOne.split(' '));
+
+
+const a = "c";
+const b = "c";
+if (a < b) {
+  console.log(`${a} is less than ${b}`);
+} else if (a > b) {
+  console.log(`${a} is greater than ${b}`);
+} else {
+  console.log(`${a} and ${b} are equal.`);
+}
+
