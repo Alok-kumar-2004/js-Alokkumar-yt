baa16d9f367d87ed59ed9a5171fd0a652072adfe
Alok-kumar-2004 <137861383+Alok-kumar-2004@users.noreply.github.com>
2024-06-20 16:38:55 +0000

    nums and maths done

---
+++ 01_basics/06_numsandmaths.js
@@ -0,0 +1,34 @@
+// const score =400;
+// console.log(score);
+
+// const balance=new Number(100.63534)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(3));
+
+// const otherNumber=1.8465
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds = 100000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'))
+
+//+++++++++++++++++++++++  Maths  +++++++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-2));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.8));
+// console.log(Math.min(4,5,6,78,54));
+// console.log(Math.max(4,5,6,78,54));
+
+// console.log(Math.random());=> gives value from 0 to 1
+//console.log(((Math.random()*100)+1).toFixed(0));  // it will garantuee that the Num will not be zero
+
+const min=10
+const max=100
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)//if +min is not added 91-100 will not come as an output
