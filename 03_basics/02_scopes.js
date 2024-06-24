03c21e678804b6efcf5e584a8898970fd57f6db0
Alok-kumar-2004 <137861383+Alok-kumar-2004@users.noreply.github.com>
2024-06-24 17:30:02 +0000

    scopes done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,10 +5,51 @@ if (true) {
     let a=10
     const b=20
     var c=30
-    console.log("INNER:",a);
+    // console.log("INNER:",a);
 }
 
- console.log("OUTER",a);
+//  console.log("OUTER",a);
 // console.log(b);
-console.log(c);// problem with var keyword..
+// console.log(c);// problem with var keyword..
 
+function one(){
+    const username="alokkumar"
+
+    function two(){
+        const website="Youtube"
+        console.log(username);
+    }
+    //console.log(website);
+
+    two()
+}
+
+// one()
+
+if (true) {
+    const username="Alokkumar"
+    if (username === "Alokkumar") {
+        const website=" youTube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// +++++++++++++++ interesting ++++++++++++++++++
+
+console.log(addOne(4))
+function addOne(num){
+    return num+1
+}
+
+console.log(addOne(4))
+
+
+//basic concept of hoisting
+// console.log(addTwo(4))=> it will throw error
+const addTwo = function(num){//another way to declare function
+    return num+2
+}
+
+console.log(addTwo(4))
