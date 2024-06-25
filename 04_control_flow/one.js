bbe1413a96dbd059c5ee73d359322157bc2af763
Alok-kumar-2004 <137861383+Alok-kumar-2004@users.noreply.github.com>
2024-06-25 17:40:25 +0000

    alok

---
+++ 04_control_flow/one.js
@@ -0,0 +1,28 @@
+// if
+const username="Alokkumar"
+const temprature=50
+const isUSerLoggedIn=true
+if(temprature>50){
+    // console.log(`Hello ${username},Welcome to the website`);
+}else{
+// console.log(`Hello ${username},You're not Welcome to the website`);
+}
+
+// console.log("!00% executed ");
+
+const score =200
+
+if(score>100){
+    // const power ="fly"=> Correct..
+    //var power ="fly"
+    console.log(`User Power :${power}`);
+}
+console.log(`User power :${power}`);
+
+
+
+const balance =1000
+if(balance>500) console.log("test");// cannot write multiple lines or use "," .
+if(balance>500) console.log("test"),
+console.log("test 2"); // => unreadable dont use thse syntax.
+
