39d3bdb604f0587c16b163aa3bc47f7c6f0ab462
Alok-kumar-2004 <137861383+Alok-kumar-2004@users.noreply.github.com>
2024-06-26 15:57:30 +0000

    loop done

---
+++ 05_Iterations.js/03_three.js
@@ -0,0 +1,14 @@
+// for of
+//["","",""]
+//[{},{},{}]
+const arr =[1,2,3,4,5]
+
+for (const val of arr) {
+    console.log(val);
+}
+
+const greetings = "Hello world !"
+for (const greet of greetings) {
+    console.log(`Each char is ${greet}`);
+}
+
